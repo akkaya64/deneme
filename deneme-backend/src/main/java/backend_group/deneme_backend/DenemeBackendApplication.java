7b5c51106bba7081a35b67d09f1ecca2da61281c
fatih <akkaya064@gmail.com>
2024-08-14 14:21:23 +0100

    backendfirstcommit

---
+++ deneme-backend/src/main/java/backend_group/deneme_backend/DenemeBackendApplication.java
@@ -0,0 +1,13 @@
+package backend_group.deneme_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenemeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenemeBackendApplication.class, args);
+	}
+
+}
---
+++ deneme-backend/src/test/java/backend_group/deneme_backend/DenemeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_group.deneme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenemeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
